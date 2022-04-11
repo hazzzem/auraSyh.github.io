178099429679ade0850bebb2aefac0bffceca6b2
auraSyh <103303543+auraSyh@users.noreply.github.com>
2022-04-11 09:22:29 +0700

    Menambahkan fitur baru

--- src/script2.js
+++ src/script2.js
@@ -12,3 +12,59 @@ button.addEventListener("click", function(){
    const b = Math.round(Math.random()*255+1);
    body.style.backgroundColor = ('rgb('+r+', '+g+', '+b+')');
 })
+
+const red = document.getElementById('red');
+const green = document.getElementById('green');
+const blue = document.getElementById('blue');
+
+function change() {
+   red.addEventListener("input", change);
+   green.addEventListener("input", change);
+   blue.addEventListener("input", change);
+   const r = red.value;
+   const g = green.value;
+   const b = blue.value;
+   body.style.backgroundColor = ('rgb('+r+', '+g+', '+b+')');
+   body.style.backgroundColor = ('rgb('+r+', '+g+', '+b+')');
+   body.style.backgroundColor = ('rgb('+r+', '+g+', '+b+')');
+}
+
+// red.addEventListener("input", function(){
+//    const r = red.value;
+//    const g = green.value;
+//    const b = blue.value;
+//    body.style.backgroundColor = ('rgb('+r+', '+g+', '+b+')');
+//    body.style.backgroundColor = ('rgb('+r+', '+g+', '+b+')');
+//    body.style.backgroundColor = ('rgb('+r+', '+g+', '+b+')');
+// });
+//
+// green.addEventListener("input", function(){
+//    const r = red.value;
+//    const g = green.value;
+//    const b = blue.value;
+//    body.style.backgroundColor = ('rgb('+r+', '+g+', '+b+')');
+//    body.style.backgroundColor = ('rgb('+r+', '+g+', '+b+')');
+//    body.style.backgroundColor = ('rgb('+r+', '+g+', '+b+')');
+// });
+//
+// blue.addEventListener("input", function(){
+//    const r = red.value;
+//    const g = green.value;
+//    const b = blue.value;
+//    body.style.backgroundColor = ('rgb('+r+', '+g+', '+b+')');
+//    body.style.backgroundColor = ('rgb('+r+', '+g+', '+b+')');
+//    body.style.backgroundColor = ('rgb('+r+', '+g+', '+b+')');
+// });
+
+change();
+
+body.addEventListener("mousemove", function(event){
+   const x = Math.round((event.clientX/window.innerWidth*255));
+   const y = Math.round((event.clientY/window.innerWidth*255));
+
+   body.style.backgroundColor = 'rgb('+x +', '+y+',100)';
+})
+
+// button.addEventListener("click", function(){
+//    document.getElementById('reset').reset();
+// })
